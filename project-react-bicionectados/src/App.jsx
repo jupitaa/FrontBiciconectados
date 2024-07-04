1f14dd799772e8673f5316e32462663f455fe765
claudioandres22 <claudiorojas122@gmail.com>
2024-07-04 16:00:01 -0400

    Tu mensaje de commit aquí

--- project-react-bicionectados/src/App.jsx
+++ project-react-bicionectados/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import LoginPage from "./pages/LoginPage";
+
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
-      <LoginPage />
+      <Header />
     </>
   );
 }
